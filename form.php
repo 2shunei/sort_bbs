85e86546906a1587614f1871c6ca855d36ca1ae0
azumaden <2501908@s.asojuku.ac.jp>
2025-06-05 10:25:24 +0900

    hahahahahaha

--- form.php
+++ form.php
@@ -8,11 +8,12 @@
 <body>
     <h1>💬 一言掲示板</h1>
     <form action="post.php" method="post">
-        <p>名前：<input type="text" name="name" required></p>
+        <?php echo "ようこそ、",$_POST['name'] ,"さん";?>
         <p>コメント：<br>
         <textarea name="comment" rows="4" cols="40" required></textarea></p>
         <p><button type="submit">投稿する</button></p>
     </form>
     <p><a href="view.php">▶ 投稿一覧を見る</a></p>
+    <p><a href="login.php">ログアウト</a></p>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+</head>
+<body>
+    <h1>ログイン</h1>
+    <form action="form.php" method="post">
+     <p>名前：<input type="text" name="name" ></p>
+      <p>パスワード：<input type="password" name="pasword" ></p>
+      <p><button type="submit">ログイン</button></p>
+</body>
+</html>
